8627529b1db180540ffaf4bc774955fd2ca11b6a
hector.frasson <hector.frasson@lemondata.com.ar>
2022-08-18 02:00:34 -0300

    Creamos el microservicio Eureka

---
+++ springboot-servicio-eureka/src/main/java/com/formacionbdi/springboot/app/eureka/SpringbootServicioEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.springboot.app.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicioEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioEurekaApplication.class, args);
+	}
+
+}
