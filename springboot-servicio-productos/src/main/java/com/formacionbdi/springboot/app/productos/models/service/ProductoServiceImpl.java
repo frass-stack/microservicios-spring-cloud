65e32dff234fb1760bf1281fc1f1aa4b674ea315
hector.frasson <hector.frasson@lemondata.com.ar>
2022-08-22 12:47:43 -0300

    CRUD en servicio productos.

--- springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/models/service/ProductoServiceImpl.java
+++ springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/models/service/ProductoServiceImpl.java
@@ -27,4 +27,16 @@ public class ProductoServiceImpl implements ProductoService{
 		return productoDao.findById(id).orElse(null);
 	}
 
+	@Override
+	@Transactional
+	public Producto save(Producto producto) {
+		return productoDao.save(producto);
+	}
+
+	@Override
+	@Transactional
+	public void deleteById(Long id) {
+		productoDao.deleteById(id);
+	}
+
 }
