23346d3d8925b9caf32f28042387f1d98b312118
hector.frasson <hector.frasson@lemondata.com.ar>
2022-08-22 14:46:05 -0300

    Modificando servicio-productos para agregar dependecy commons. Adicionalmente en el servicio commons se quita la configuracion de H2 Database.

--- springboot-servicio-commons/src/main/java/com/formacionbdi/springboot/app/commons/SpringbootServicioCommonsApplication.java
+++ springboot-servicio-commons/src/main/java/com/formacionbdi/springboot/app/commons/SpringbootServicioCommonsApplication.java
@@ -1,8 +1,15 @@
-package com.formacionbdi.springboot.app.commons;
+package com.formacionbdi.springboot.app.productos;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
-public class SpringbootServicioCommonsApplication {
+public class SpringbootServicioProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioProductosApplication.class, args);
+	}
 
 }
