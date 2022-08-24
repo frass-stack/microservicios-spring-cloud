775ec5fdadc2ca845aec854adaf727675a4afa42
hector.frasson <hector.frasson@lemondata.com.ar>
2022-08-24 14:09:10 -0300

    Añadiendo la configuracion para el servidor de autorizacion en servicio oauth.

--- springboot-servicio-oauth/src/main/java/com/formacionbdi/springboot/app/oauth/security/SpringSecurityConfig.java
+++ springboot-servicio-oauth/src/main/java/com/formacionbdi/springboot/app/oauth/security/SpringSecurityConfig.java
@@ -16,7 +16,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	private UserDetailsService usuarioService;
 	
 	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
+	public static BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 	
