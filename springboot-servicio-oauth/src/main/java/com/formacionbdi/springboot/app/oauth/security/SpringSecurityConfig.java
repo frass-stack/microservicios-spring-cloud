e82f4a8156136b21ba1cad3f4ce4adbf35ef736b
hector.frasson <hector.frasson@lemondata.com.ar>
2022-08-26 16:55:57 -0300

    Manejando eventos de exito y fracaso en la autenticacion.

--- springboot-servicio-oauth/src/main/java/com/formacionbdi/springboot/app/oauth/security/SpringSecurityConfig.java
+++ springboot-servicio-oauth/src/main/java/com/formacionbdi/springboot/app/oauth/security/SpringSecurityConfig.java
@@ -3,6 +3,7 @@ package com.formacionbdi.springboot.app.oauth.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationEventPublisher;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,15 +16,20 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private UserDetailsService usuarioService;
 	
+	@Autowired
+	private AuthenticationEventPublisher eventPublisher;
+	
 	@Bean
 	public static BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 	
 	@Override
+	@Autowired
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(usuarioService).passwordEncoder(passwordEncoder());
-		super.configure(auth);
+		auth.userDetailsService(usuarioService).passwordEncoder(passwordEncoder())
+		.and()
+		.authenticationEventPublisher(eventPublisher);
 	}
 
 	@Override
---
+++ springboot-servicio-oauth/src/main/java/com/formacionbdi/springboot/app/oauth/security/events/AuthenticationSuccessErrorHandler.java
@@ -0,0 +1,40 @@
+package com.formacionbdi.springboot.app.oauth.security.events;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.AuthenticationEventPublisher;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.stereotype.Component;
+
+import com.formacionbdi.springboot.app.oauth.services.UsuarioService;
+
+@Component
+public class AuthenticationSuccessErrorHandler implements AuthenticationEventPublisher{
+	
+	private Logger log = LoggerFactory.getLogger(AuthenticationSuccessErrorHandler.class);
+
+	@Override
+	public void publishAuthenticationSuccess(Authentication authentication) {
+		
+		if(authentication.getDetails() instanceof WebAuthenticationDetails) {
+			return;
+		}
+		
+		UserDetails user = (UserDetails) authentication.getPrincipal();
+		String mensaje = "Success Login: " + user.getUsername();
+		System.out.println(mensaje);
+		log.info(mensaje);
+	}
+
+	@Override
+	public void publishAuthenticationFailure(AuthenticationException exception, Authentication authentication) {
+		String mensaje = "Error en el Login: " + exception.getMessage();
+		System.out.println(mensaje);
+		log.info(mensaje);
+		
+	}
+
+}
